a0210adf1de4ecc719a3f08c96437340ce7e4cf3
lyankey <56731939+lyankey@users.noreply.github.com>
2020-10-15 11:59:33 -0400

    fifth

--- ElevenNote.Models/NoteDetail.cs
+++ ElevenNote.Models/NoteDetail.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    public class NoteListItem
+    public class NoteDetail
     {
         public int NoteId { get; set; }
         public string Title { get; set; }
-
+        public string Content { get; set; }
         [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
--- ElevenNote.Models/NoteService.cs
+++ ElevenNote.Models/NoteService.cs
@@ -47,6 +47,25 @@ public class NoteService
             return query.ToArray();
         }
     }
+    public NoteDetail GetNoteById(int id)
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var entity =
+                ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+            return
+                new NoteDetail
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };
+        }
+    }
     private readonly Guid _userId;
 
     public NoteService(Guid userId)
